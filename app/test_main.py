8d388784d641ceddcce93a99fad9cf2d4999a1c3
fanghey <2603Lysenko2603@gmail.com>
2026-05-21 19:44:37 +0200

    Update test_main.py

--- app/test_main.py
+++ app/test_main.py
@@ -1,19 +1,28 @@
+# app/test_main.py
+
 import pytest
 from main import get_human_age
 
 
-@pytest.mark.parametrize("cat,dog,expected", [
-    (0, 0, [0, 0]),
-    (14, 14, [0, 0]),
-    (15, 15, [1, 1]),
-    (23, 23, [1, 1]),
-    (24, 24, [2, 2]),
-    (27, 27, [2, 2]),
-    (28, 28, [3, 2]),
-    (100, 100, [21, 17]),
-    # різні значення
-    (28, 25, [3, 2]),
-    (25, 28, [2, 3]),
-])
-def test_get_human_age(cat, dog, expected):
+@pytest.mark.parametrize(
+    ("cat", "dog", "expected"),
+    [
+        (0, 0, [0, 0]),
+        (14, 14, [0, 0]),
+        (15, 15, [1, 1]),
+        (23, 23, [1, 1]),
+        (24, 24, [2, 2]),
+        (27, 27, [2, 2]),
+        (28, 28, [3, 2]),
+        (100, 100, [21, 17]),
+        # різні значення
+        (28, 25, [3, 2]),
+        (25, 28, [2, 3]),
+    ],
+)
+def test_get_human_age(
+    cat: int,
+    dog: int,
+    expected: list[int],
+) -> None:
     assert get_human_age(cat, dog) == expected
